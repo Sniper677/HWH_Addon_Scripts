f31a44adda1b65694418dff79ca30f816ce6b7f0
Sniper677 <sniper677@arcor.de>
2025-12-06 12:42:49 +0100

    small code cleanup, adding/renaming files to make auto update work

---
+++ HWHRepeatCountExt.meta.js
@@ -0,0 +1,6 @@
+// ==UserScript==
+// @name            HWHRepeatCountExt
+// @version         0.0.4.2
+// @downloadURL     https://github.com/Sniper677/HWH_Addon_Scripts/raw/refs/heads/main/HWHRepeatCountExt.user.js
+// @updateURL       https://github.com/Sniper677/HWH_Addon_Scripts/raw/refs/heads/main/HWHRepeatCountExt.meta.js
+// ==/UserScript==
